156b6a602b7f2b3ae3d959dadd0de782c473c322
Johan van der Klift <info@johanvanderklift.nl>
2017-07-29 12:27:47 +0000

    Trying to get the mail confirmation work in production 1

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -9,7 +9,7 @@ Rails.application.configure do
   # Do not eager load code on boot.
   config.eager_load = false
   config.action_mailer.delivery_method = :test
-  config.action_mailer.default_url_options = { :host => 'http://rubyonrailscourse2-klippie.c9users.io:8080' }
+  config.action_mailer.default_url_options = { :host => 'https://rubyonrailscourse2-klippie.c9users.io' }
 
   # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
