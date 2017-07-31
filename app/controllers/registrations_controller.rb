8d29a7a0586a405ec437ffe558d22542b24a31eb
Johan van der Klift <info@johanvanderklift.nl>
2017-07-31 13:05:21 +0000

    Stripe bug fix 1

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,33 +1,33 @@
 class RegistrationsController < Devise::RegistrationsController
-  
-  def reate
+  def create
     build_resource(sign_up_params)
     
     resource.class.transaction do
-      recource.save
-      yield recource if block_given?
+      resource.save
+      yield resource if block_given?
       if resource.persisted?
-        @payment = Payment.new({ email: params["user"]["email"], token: params[:payment]["token"], user_id: resource.id})
+        @payment = Payment.new({ email: params["user"]["email"], 
+          token: params[:payment]["token"], user_id: resource.id })
         
         flash[:error] = "Please check registration errors" unless @payment.valid?
         
         begin
           @payment.process_payment
           @payment.save
-        rescue Exception => e
+        rescue Exception => e 
           flash[:error] = e.message
           
-          recource.destroy
+          resource.destroy
           puts 'Payment failed'
           render :new and return
         end
         
         if resource.active_for_authentication?
-          set_flash_message! :notice, :signed_up
+          set_flash_message :notice, :signed_up if is_flashing_format?
           sign_up(resource_name, resource)
           respond_with resource, location: after_sign_up_path_for(resource)
         else
-          set_flash_message! :notice, :"signed_up_but_#{resource.inactive_message}"
+          set_flash_message :notice, :"signed_up_but_#{resource.inactive_message}" if is_flashing_format?
           expire_data_after_sign_in!
           respond_with resource, location: after_inactive_sign_up_path_for(resource)
         end
@@ -44,5 +44,4 @@ class RegistrationsController < Devise::RegistrationsController
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up).push(:payment)
   end
-  
 end
--- app/models/payment.rb
+++ app/models/payment.rb
@@ -1,9 +1,9 @@
 class Payment < ActiveRecord::Base
-  attr_accessor :car_number, :card_ccv, :card_expires_month, :card_expires_year
+  attr_accessor :card_number, :card_cvv, :card_expires_month, :card_expires_year
   belongs_to :user
   
   def self.month_options
-    Date::MONTHNAMES.compact.each_with_index.map { |name, i| [ "#{i+1} - #{name}", i+1] }
+    Date::MONTHNAMES.compact.each_with_index.map { |name, i| ["#{i+1} - #{name}", i+1]}
   end
   
   def self.year_options
@@ -13,9 +13,7 @@ class Payment < ActiveRecord::Base
   def process_payment
     customer = Stripe::Customer.create email: email, card: token
     
-    Stripe::Charge.create customer: customer.id,
-                          amount: 1000,
-                          description: 'Premium',
-                          currency: 'usd'
+    Stripe::Charge.create customer: customer.id, amount: 1000, description: 'Premium', currency: 'usd'
   end
+  
 end
